7170534037ac2cbf39fbef6ffaad5d90d2583db6
yxfcodeup <825378343@qq.com>
2017-04-12 11:26:05 +0800

    del dis...

--- distributedSpiderFrame/src/tools/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-import sys
-PYVI = sys.version_info
-__all__ = [ "Platform" , "BS4Spider" , "MultiProcess" , "Proxy" , "SeleniumSpider" , "MD5" , "StringTool" , "Log" , "Network" , "RequestsSpider" , "ListTool"]
